d65ccf4084036ef3c179907d03516bb61e68d4f8
kkayam <korre.remix@gmail.com>
2024-01-06 20:27:29 +0100

    added blog posts

--- components/BlogPage.js
+++ components/BlogPage.js
@@ -1,5 +1,18 @@
 'use client';
 import styles from '@/app/page.module.css';
 
+var post_titles = ["Worms are cool, and heres why", "Japanese head massage", "Volcano eruption, and how this affects us", "Develop a game - NOW"];
+
 export default function BlogPage() {
+    return (<>
+        {post_titles.map((title) => {
+            return (<div className={styles.blogPostThumb}>
+                <div style={{ float: "left" }}>
+                    <div className={styles.blogPostTitle}>{title}</div>
+                    <div className={styles.blogPostViews}>123,333 views</div>
+                </div>
+                <div className={styles.blogPostTime}>~3min</div>
+            </div>);
+        })}
+    </>);
 }
