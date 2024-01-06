3b9da7a9fe1a4fec9928558615c4e3e840c17056
kkayam <korre.remix@gmail.com>
2024-01-06 16:56:21 +0100

    buttons functional and title

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
-import Image from 'next/image';
 import styles from './page.module.css';
+import NavBar from '@/components/NavBar';
 
 export default function Home() {
   return (
@@ -7,22 +7,13 @@ export default function Home() {
       <div className={styles.container}>
         <NavBar />
         <div className={styles.title}>
-          hey, im koray
+          hey, im koray ✌️
         </div>
 
-        <div className={styles.section}>Project 1</div>
-        <div className={styles.section}>Project 2</div>
-        <div className={styles.section}>Project 3</div>
+        <div className={styles.section} id='Home'>Project 1</div>
+        <div className={styles.section} id='Resume'>Project 2</div>
+        <div className={styles.section} id='Portfolio'>Project 3</div>
       </div>
     </main>
   );
-}
-
-
-function NavBar() {
-  return (<div className={styles.navbar}>
-    <div className={styles.navbarButton}>Home</div>
-    <div className={styles.navbarButton}>Resume</div>
-    <div className={styles.navbarButton}>Portfolio</div>
-  </div>);
 }
---
+++ components/NavBar.js
@@ -0,0 +1,20 @@
+'use client';
+import styles from '@/app/page.module.css';
+
+export default function NavBar() {
+    const scrollToElement = (event) => {
+        const button = event.target;
+        const targetId = button.innerHTML.trim();
+        const element = document.getElementById(targetId);
+
+        if (element) {
+            element.scrollIntoView({ behavior: 'smooth' });
+        }
+    };
+
+    return (<div className={styles.navbar}>
+        <div className={styles.navbarButton} onClick={scrollToElement}>Home</div>
+        <div className={styles.navbarButton} onClick={scrollToElement}>Resume</div>
+        <div className={styles.navbarButton} onClick={scrollToElement}>Portfolio</div>
+    </div>);
+}
